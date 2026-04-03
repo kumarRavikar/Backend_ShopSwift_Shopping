60fcf00a41185b88df59bd58479c889782fbb3eb
kumar ravikar <kumarravikar24@gmail.com>
2026-04-03 20:39:53 +0530

    adding origin for connect backend

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://shopswiftshoppingmernstack.vercel.app/",
   credentials:true
 }));
 
