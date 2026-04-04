bafbb28ce2e585d775c444bbf3ef5e1aa9751d49
kumar ravikar <kumarravikar24@gmail.com>
2026-04-04 15:55:53 +0530

    fixing db

--- DataBase/db.js
+++ DataBase/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import 'dotenv/config'
 const connectDB  = async()=>{
     try {
-        await mongoose.connect(`${process.env.MONGO_URI}/shopswift`)
+        await mongoose.connect(`${process.env.MONGO_URI}`)
         console.log('Database Connected Successfully')
     } catch (error) {
         console.log("DataBase connection faild : " , error)
