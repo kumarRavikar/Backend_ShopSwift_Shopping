f3f7c2ad9cb30ff4b411ced278aa45dee1e7ac74
kumar ravikar <kumarravikar24@gmail.com>
2026-07-23 15:52:01 +0530

    fixing bug in emailVerify

--- EmailVerify/emailVeify.js
+++ EmailVerify/emailVeify.js
@@ -1,52 +1,51 @@
-
 import "dotenv/config";
-import {Resend} from "resend";
-const resend = new Resend(process.env.RESEND_API)
+import { Resend } from "resend";
+const resend = new Resend(process.env.RESEND_API);
 const verifyEmail = async (token, email) => {
-//   try {
-//     const transporter = nodemailer.createTransport({
-//       host: "smtp.gmail.com",
-//       port: 587,
-//       secure: false,
-//       auth: {
-//         user: process.env.MAIL_USER,
-//         pass: process.env.MAIL_PASS,
-//       },
-//     });
-//      await transporter.verify();
-//      console.log("SMTP is working");
-//     const verificationLink = `https://shopswiftshoppingmernstack.vercel.app/verify/${token}`;
+  //   try {
+  //     const transporter = nodemailer.createTransport({
+  //       host: "smtp.gmail.com",
+  //       port: 587,
+  //       secure: false,
+  //       auth: {
+  //         user: process.env.MAIL_USER,
+  //         pass: process.env.MAIL_PASS,
+  //       },
+  //     });
+  //      await transporter.verify();
+  //      console.log("SMTP is working");
+  //     const verificationLink = `https://shopswiftshoppingmernstack.vercel.app/verify/${token}`;
 
-//     const mailConfig = {
-//       from: process.env.MAIL_USER,
-//       to: email,
-//       subject: "Email Verification",
-//       html: `
-//         <h2>Email Verification</h2>
-//         <p>Click the link below to verify your account:</p>
-//         <a href="${verificationLink}">Verify Email</a>
-//       `,
-//     };
+  //     const mailConfig = {
+  //       from: process.env.MAIL_USER,
+  //       to: email,
+  //       subject: "Email Verification",
+  //       html: `
+  //         <h2>Email Verification</h2>
+  //         <p>Click the link below to verify your account:</p>
+  //         <a href="${verificationLink}">Verify Email</a>
+  //       `,
+  //     };
 
-//     await transporter.sendMail(mailConfig);
-//         console.log("RENDER MAIL:", process.env.MAIL_USER);
-// console.log("RENDER PASS:", process.env.MAIL_PASS ? "YES" : "NO");
-//     console.log("Verification email sent successfully");
-//   } catch (error) {
-//     console.log("Email sending failed:", error.message);
-//   }
-try {
-    const verificationLink =`https://shopswiftshoppingmernstack-ten.vercel.app/#/verify/${token}`;
+  //     await transporter.sendMail(mailConfig);
+  //         console.log("RENDER MAIL:", process.env.MAIL_USER);
+  // console.log("RENDER PASS:", process.env.MAIL_PASS ? "YES" : "NO");
+  //     console.log("Verification email sent successfully");
+  //   } catch (error) {
+  //     console.log("Email sending failed:", error.message);
+  //   }
+  try {
+    const verificationLink = `https://shopswiftshoppingmernstack-ten.vercel.app/#/verify/${token}`;
 
     await resend.emails.send({
       from: "onboarding@resend.dev",
       to: email,
       subject: "Email Verification",
       html: `
-        <h2>Email Verification</h2>
-        <p>Click below:</p>
-        <a href="${verificationLink}">Verify Email</a>
-      `,
+      <h2>Email Verification</h2>
+      <p>Click below to verify your account:</p>
+      <a href="${verificationLink}">Verify Email</a>
+    `,
     });
 
     console.log("Email sent successfully");
